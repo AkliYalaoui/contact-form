22dd484e4b34db4ca580ca3d769f0afcc22503bb
AkliYalaoui <64916075+AkliYalaoui@users.noreply.github.com>
2020-07-05 19:16:47 +0100

    added the main form and basic styles with css

--- index.php
+++ index.php
@@ -10,6 +10,19 @@
   </head>
   <body>
 
+     <!--  start form -->
+      <div class="container">
+         <h1>Contact Us</h1>
+          <form class="" action="index.html" method="post">
+              <input type="text" name="username" autocomplete="off" placeholder="Type Your Username">
+              <input type="email" name="email"  autocomplete="off" placeholder="Please Type a Valid Email">
+              <input type="text" name="mobile"  autocomplete="off" placeholder="Type Your Cell phone">
+              <textarea name="message" placeholder="Your Message !"></textarea>
+              <input type="submit"  value="Send Message">
+          </form>
+      </div>
+     <!--  end form -->
+
      <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
   </body>
 </html>
